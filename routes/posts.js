9e3df79973541b00d3ac0835a9f2666a05c92f29
Justin (Jiushan) Yang <jyang@sysomos.com>
2016-07-13 14:10:29 -0400

    1. get post by id 2. handle the case of the GET request when postedBy field of the post is empty

--- routes/posts.js
+++ routes/posts.js
@@ -2,14 +2,35 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-/* GET users listing. */
+/* GET all posts. */
 router.get('/', function(req, res, next) {
     mongoose.model('post').find({}).populate('postedBy').exec(function(err, posts){
         response_str = ''
         for (i = 0; i < posts.length; ++i) {
-            response_str = response_str + posts[i].content + ' posted by: ' + posts[i].postedBy.username + ' ';
+            response_str = response_str + posts[i].content + ' posted by: ';
+            if (posts[i].postedBy) { // if the postBy field of the post is not empty
+                response_str = response_str + posts[i].postedBy.username + ' ';
+            }
+            response_str = response_str + '<br/>';
         }
         res.send(response_str)
+    });
+});
+
+/* GET post by id. */
+router.get('/:id', function(req, res, next) {
+    var id = req.params.id;
+    mongoose.model('post').findOne({'_id': id}).populate('postedBy').exec(function(err, post){
+        if (err) {
+            res.send('cannot find id');
+        } else {
+            var response_str = post.content + ' posted by: ';
+            if (post.postedBy) { // if the postBy field of the post is not empty
+                response_str = response_str + post.postedBy.username + ' ';
+            }
+            res.send(response_str);
+        }
+
     });
 });
 
