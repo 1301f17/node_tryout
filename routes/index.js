d75944f2fe16afe9984ecaf2426fb605bf299435
Justin (Jiushan) Yang <jyang@sysomos.com>
2016-07-29 17:02:16 -0400

    call python from node js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var PythonShell = require('python-shell');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' , isAuthenticated: req.isAuthenticated(), user: req.user, flash: req.flash('error')});
+});
+
+router.get('/python', function(req, res, next) {
+  var options = {
+    scriptPath: __dirname + '/..'
+  };
+  PythonShell.run('script.py', options, function (err, results) {
+    if (err) throw err;
+    if (results){ // results is an array containing the printing outputs from python
+      res.send(JSON.parse(results[0])['name']);
+    }
+  });
 });
 
 module.exports = router;
---
+++ script.py
@@ -0,0 +1,2 @@
+print('{"name": "justin"}')
+print('{"name": "luffy"}')
